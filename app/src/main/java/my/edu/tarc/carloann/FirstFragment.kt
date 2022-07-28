7ccdb780b43042798a6b51336dd169e93b42f1e8
anreh <anurehka@gmail.com>
2022-07-28 13:38:00 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/carloann/FirstFragment.kt
@@ -0,0 +1,45 @@
+package my.edu.tarc.carloann
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import my.edu.tarc.carloann.databinding.FragmentFirstBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    //From program Run
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root //return to main activity
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+//to program exit
+    override fun onDestroyView() { // destroy all the memory allocated when switching to different page
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/my/edu/tarc/carloann/InterestData.kt
@@ -0,0 +1,4 @@
+package my.edu.tarc.carloann
+
+//TODO 2: Define a data class
+class InterestData (var bank: String="", var interest: String="")
